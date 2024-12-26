42c441490d76d484159f593a1c627985a0a99052
regepower <153322245+regepower@users.noreply.github.com>
2024-12-26 20:50:12 +0100

    Update Backup_TNC.bat

--- Backup_TNC.bat
+++ Backup_TNC.bat
@@ -7,15 +7,15 @@ SET _DEBUG=
 ::------------------------------------------------------------------------------
 ::---  ACHTUNG! Bei dem SET Befehl Leerzeichen vor und nach = weglassen!     ---
 ::---  auch keine Lehrzeichen hinter dem Dateinamen und keine "              ---
-::---  LINUX/BASH Befehle gawk.exe sowie touch.exe werden benötigt.          ---
+::---  LINUX/BASH Befehle gawk.exe sowie touch.exe werden benÃ¶tigt.          ---
 ::---  Download unter: https://sourceforge.net/projects/unxutils/            ---
 ::---  Dateien liegen im ZIP-Archiv unter usr\local\wbin\                    ---
-::---  FUER WINDOWS wird rush.exe als Alternative für xargs benötigt         ---
+::---  FUER WINDOWS wird rush.exe als Alternative fÃ¼r xargs benÃ¶tigt         ---
 ::---  Download unter: https://github.com/shenwei356/rush/releases/          ---
 ::---  7ZIP wird auch benoetigt, Download: https://7-zip.de/download.html    ---
-::---  Die ganzen ESC Sequencen für den echo Befehl findet man unter:        ---
+::---  Die ganzen ESC Sequencen fÃ¼r den echo Befehl findet man unter:        ---
 ::---  https://learn.microsoft.com/de-de/windows/console/console-virtual-terminal-sequences
-::---                                                                        ---
+::---                                                                          ---
 ::---  SYNTAX:                                                               ---
 ::---  Backup_TNC.bat [IP-Adresse / Hostname] [Archivpfad]                   ---
 ::---                                                                        ---
@@ -27,10 +27,10 @@ SET _DEBUG=
 ::---  V0.2 2022 DIFF.LST bauen mit sort + comm + findstr durch awk ersetzt  ---
 ::---  V0.3 2022 TIMESTAMP setzen komplett erneuert mit awk und rush         ---
 ::---  V0.4 2022 Backupdauer hinzu und etwas Farbe bei der Ausgabe           ---
-::---  V0.5 2022 Versuch mit chcp wegen ÄÖÜ                                  ---
+::---  V0.5 2022 Versuch mit chcp wegen Ã„Ã–Ãœ                                  ---
 ::---            FEHLER MIT & ZEICHEN IM NAMEN BEHOBEN                       ---
 ::--- BUG:                                                                   ---
-::--- BEIM DATUM SETZEN MIT TOUCH WERDEN DATEINAMEN MIT ÄÖÜ NICHT GESETZT    ---
+::--- BEIM DATUM SETZEN MIT TOUCH WERDEN DATEINAMEN MIT Ã„Ã–Ãœ NICHT GESETZT    ---
 ::--- TODO:                                                                  ---
 ::--- BUGS BEI DER AUSGABE BEHEBEN - NUR OPTIK                               ---
 ::--- FILTERLISTEN ERSTELLEN                                                 ---
